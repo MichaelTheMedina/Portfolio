461cadfc2d41977bb85ead9aca860aa0b802a8de
Michael <michaelthemedina@gmail.com>
2025-11-30 18:04:37 +0000

    Updated Favicon

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -35,7 +35,7 @@ function AboutCard() {
             </li>
           </ul>
 
-          <p style={{ color: "rgb(155 126 172)" }}>
+          <p style={{ color: "rgb(0 255 0)" }}>
             "Strive to build things that make a difference!"{" "}
           </p>
           <footer className="blockquote-footer">Michael Medina</footer>
